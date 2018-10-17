acd6d36de30fa90f5a2698086f86b672f39b31f6
yyshi <yyshi@mobvoi.com>
2018-10-17 16:14:45 -0700

    add softlink for conf

--- egs/wsj/path.sh
+++ egs/wsj/path.sh
@@ -15,4 +15,5 @@ export PATH=$EESEN_ROOT/src/netbin:$EESEN_ROOT/src/featbin:$EESEN_ROOT/src/decod
 [ ! -e steps ] && cp -r  $KALDI_ROOT/egs/wsj/s5/steps ./ && cp $EESEN_ROOT/asr_egs/wsj/steps/* ./steps/ 
 [ ! -e scripts ] && ln -sf ../../scripts ./
 [ ! -e utils ] && cp -r  $KALDI_ROOT/egs/wsj/s5/utils ./ && cp $EESEN_ROOT/asr_egs/wsj/utils/* ./utils/ 
+[ ! -e conf ] && ln -sf ../../conf ./
 return 0
